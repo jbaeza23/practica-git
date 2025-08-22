670eff3887e0f7f9f8ba2189cdb6153f44425650
Jose Rafael <jbaeza_23@alu.uabcs.mx>
2025-08-22 08:13:02 -0700

    se realizo y agrego la practica.dart

---
+++ practica.dart
@@ -0,0 +1,22 @@
+void main() {
+  print("Suma: ${suma(2, 2)}");
+  print("Resta: ${resta(15, 5)}");
+  print("Multiplicación: ${multiplicacion(5, 3)}");
+  print("División: ${division(15, 3)}");
+}
+
+double suma(double a, double b) {
+  return a + b;
+}
+
+double resta(double a, double b) {
+  return a - b;
+}
+
+double multiplicacion(double a, double b) {
+  return a * b;
+}
+
+double division(double a, double b) {
+  return a / b;
+}
